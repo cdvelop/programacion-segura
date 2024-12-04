890545af5a4f5c4ed81ba15b25ad1ebdef57013b
César Solís <44058491+cdvelop@users.noreply.github.com>
2024-12-04 13:35:47 +0000

    varios print

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 
 func main() {
 
-	fmt.Println("hola mundo")
+	var mundo = "Mundo ok"
+	//fmt.Println("hola mundo")
+	fmt.Print("hola mundo")
+	fmt.Printf("hola %v\n", mundo)
+	fmt.Println("hello")
 
 }
-
